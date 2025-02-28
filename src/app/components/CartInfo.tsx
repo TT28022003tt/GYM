c7b4488452ecba35c388d703410d338605a26e3d
Thanh Tâm <tamle6797@gmail.com>
2025-02-28 07:54:31 +0700

    First commit

--- src/app/components/CartInfo.tsx
+++ src/app/components/CartInfo.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image";
 import QuantitySelector from "./QuantitySelector";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
--- src/app/components/CheckoutBar.tsx
+++ src/app/components/CheckoutBar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 const CheckoutBar = () => {
   return (
 	<div className=''>CheckoutBar</div>
